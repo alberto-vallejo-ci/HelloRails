ccf7c77a2621c49f97dab36676d8377eca23e844
alberto-vallejo-ci <alberto.vallejo.ci@crowdint.com>
2012-07-27 14:00:03 -0500

    First Commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloRails::Application.config.secret_token = 'fbcebe3029a2dddb3f40f5aa43b4117c9e722dae3d51ca88b1ef2b25c056e1bf4f5295539dece04857e75a9b1a005835da14ad5d0b44b9ec70bedf5f2b2e1372'
